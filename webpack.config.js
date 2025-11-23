85356e5359aa91731452f80c409ab50ee39d6f47
MILANA2003Z <96334845+MILANA2003Z@users.noreply.github.com>
2025-11-23 11:23:27 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
-
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  entry: './src/index.js',
+
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
   module: {
     rules: [
       {
@@ -27,15 +30,17 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          {loader: 'css-loader'}
+          MiniCssExtractPlugin.loader,
+          'css-loader',
         ],
       },
     ],
   },
+
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
-      filename: './index.html',
+      filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
