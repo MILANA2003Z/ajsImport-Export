0d2c0b17619e885df79288f8ae577f1fbe72b757
MILANA2003Z <96334845+MILANA2003Z@users.noreply.github.com>
2025-11-23 11:15:37 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,45 @@
+
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js', // точка входа
   output: {
-    filename: 'bundle.js', // итоговый файл
     path: path.resolve(__dirname, 'dist'),
-    clean: true, // очищает dist перед сборкой
   },
-  mode: 'development', // режим по умолчанию
-  devtool: 'inline-source-map', // для удобной отладки
   module: {
     rules: [
       {
-        test: /\.txt$/i,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.js$/i, // транспиляция через Babel
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'css-loader'}
+        ],
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new HtmlWebPackPlugin({
       template: './src/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    port: 8080,
-    open: true,
-    hot: true,
-  },
 };
