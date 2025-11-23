cd90ca34a4f965e9ae0d8daa1a357853a64bee5a
MILANA2003Z <96334845+MILANA2003Z@users.noreply.github.com>
2025-11-23 11:34:19 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,19 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  // Точка входа
   entry: './src/index.js',
 
+  // Точка выхода
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // очищает dist перед каждой сборкой
   },
 
   module: {
     rules: [
+      // JS через Babel
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -19,6 +23,7 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      // HTML
       {
         test: /\.html$/,
         use: [
@@ -27,6 +32,7 @@ module.exports = {
           },
         ],
       },
+      // CSS
       {
         test: /\.css$/,
         use: [
@@ -47,4 +53,16 @@ module.exports = {
       chunkFilename: '[id].css',
     }),
   ],
+
+  // Настройки devServer для npm start
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+
+  mode: 'development', // по умолчанию, можно менять на production при сборке
 };
